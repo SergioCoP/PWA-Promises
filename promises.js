63213de9b1447e66fd77a13628c224ef57d3b469
Francisco Javier Garcia Espinoza <20203tn021@utez.edu.mx>
2023-09-11 17:55:54 -0600

    [ADD] Promesas basicas

--- promises.js
+++ promises.js
@@ -1,3 +1,20 @@
-const hola = () =>{
-    console.log("Hola, te pones bien cachonda")
+let result = undefined;
+
+console.log(result)
+
+const waitUntil = (message) =>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+               result = message;
+               reject(result); 
+        },2000)
+    });
 }
+
+waitUntil('Puras promesas de ella').then(result=>{
+    console.log(result)
+}).catch(err=>{
+    console.log(err);
+});
+
+console.log(result)
