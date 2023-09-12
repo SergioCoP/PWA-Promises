1f20f28f57f63a9031e6f476d2a65f990c119d15
20203tn125 <20203tn125@utez.edu.mx>
2023-09-11 18:47:54 -0600

    [ADD] Ejercicios promesas

---
+++ Ejercicios1Promise.js
@@ -0,0 +1,51 @@
+//Ejercicio 1
+let nAle = 0;
+const numAleatorio = () =>{
+    return new Promise((res, rej)=>{
+        setTimeout(()=>{
+                res(Math.floor(Math.random() * 100) ,0)
+        })
+    })
+}
+
+
+const nCuadrado = (n) =>{
+    return new Promise((res,rej)=>{
+        setTimeout(()=>{
+            res(n^2)
+        },3000)
+    })
+}
+
+
+
+const nRaiz = (n) =>{
+    return new Promise((res, reject) => {
+        setTimeout(()=>{
+            res(Math.sqrt(n))
+        },1000)
+    })
+}
+
+numAleatorio().then(res=>{
+    console.log(res);
+    nCuadrado(res).then(res=>{
+        console.log(res);
+        nRaiz(res).then(res=>{
+            console.log(res);
+        }).catch(err=>{
+            console.log(err)
+        })
+    }).catch(err=>{
+        console.log(err)
+    })
+}).catch(err =>{
+    console.log(err)
+})
+
+//Ejercicio 2
+
+
+const urls = ['https://reqres.in/api/users?page=2',
+'https://pokeapi.co/api/v2/pokemon','https://jsonplaceholder.typicode.com/users'
+]
--- promises.js
+++ promises.js
@@ -1,11 +1,11 @@
 let result = undefined;
 
-const waitUntil = (message) =>{
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-               result = message;
-               reject(result); 
-        },2000)
+const waitUntil = (message) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            result = message;
+            reject(result);
+        }, 2000)
     });
 }
 
@@ -15,26 +15,52 @@ const waitUntil = (message) =>{
 //     console.log(err);
 // });
 
-const multiplicar = (n1,n2) =>{
-return new Promise((resolve,reject) =>{
-    setTimeout(()=>{
+const multiplicar = (n1, n2) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
             resolve(n1 * n2)
-    },1500)
-})
+        }, 1500)
+    })
 }
 
 
 
-const dividir = (n1,n2) =>{
-    return new Promise((resolve,reject) =>{
-        setTimeout(()=>
-            resolve(n1 / n2),2000
+const dividir = (n1, n2) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() =>
+            resolve(n1 / n2), 2000
         );
     })
-    }
+}
 
-    //Ejecute todas las promesas que se envien
-    //falla una, solo envia la excepcion de la que fallo.
+
+const multiplicarLento = (n1, n2) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>
+            resolve(n1 * n2), 2000)
+    })
+}
+
+
+
+const dividirRapido = (n1, n2) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() =>
+            resolve(n1 / n2), 2000)})
+
+}
+
+//Devuelve el resultado de la primer promesa en resolverse
+//Si una falla y es mas rapida que las demás, devuelve la escepción o manda el error de esa.
+
+// Promise.race([multiplicarLento(1,2),dividirRapido(1,2)]).then((res=>{
+//     console.log('THEN ', res)
+// })).catch(err => {
+//     console.log('error', err);
+// })
+
+//Ejecute todas las promesas que se envien
+//falla una, solo envia la excepcion de la que fallo.
 // Promise.all([multiplicar(1,2),dividir(1,3),'Promesa aqui',35]).then((result)=>{
 //     console.log("then ", result);
 // }).catch((err)=>{
